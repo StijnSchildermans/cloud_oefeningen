198cd10bb302bab01345205ef149431d53cc3df8
Stijn Schildermans <stijn.schildermans@kuleuven.be>
2020-12-29 19:34:07 +0000

    frontend with Flask

---
+++ layout/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template
+from flask import request
+import time as t
+import requests
+import json
+from threading import Thread
+
+
+
+app = Flask(__name__)
+
+
+def get_rows(start, end, buf):
+    triangle = json.loads(requests.get('http://172.17.0.1:3000/pascal.php?start=' + str(start) + '&end=' + str(end)).content)
+    for i in range(start,end):
+        buf[i] = triangle[i - start]
+
+
+
+@app.route('/')
+def build_triangle():
+    start = t.time()
+    rows = int(request.args.get('rows'))
+    res = [None] * rows
+    t1 = Thread(target = get_rows, args = (0,rows-1,res))
+    t1.start()
+    get_rows(rows-1,rows,res)
+    t1.join()
+
+
+    end = t.time()
+    time_passed = end - start
+    return render_template('index.html',rows=rows,triangle=res,time=time_passed)
+
+if __name__ == '__main__':
+    app.run()
